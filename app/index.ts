eab3b5eef52307aa2b1b298d79ef14004dac1189
JaganKaartik <kaartikjagan@gmail.com>
2020-10-10 21:33:58 +0530

    Added Basic EJS Templates

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 
 const app = express()
+const path = require('path')
+
+app.set('views',path.join(__dirname,'./views'))
 
 app.get('/',(req,res)=>{
     res.send('<h1>Welcome to My Express App</h1>')
