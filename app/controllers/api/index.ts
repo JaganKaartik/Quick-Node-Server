6481f1e37835bb64d92c1ccc0611658abe1e8a3b
JaganKaartik <kaartikjagan@gmail.com>
2020-10-14 18:04:16 +0530

    Added API Controllers --Merged DIR

---
+++ app/controllers/api/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const router = express.Router()
+
+const getController = (req,res) =>{
+    /* Logic */
+}
+
+const postController = (req,res) =>{
+    /* Logic */
+}
+
+const deleteController = (req,res) =>{
+    /* Logic */
+}
+
+const putController = (req,res) =>{
+    /* Logic */
+}
