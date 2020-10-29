c3832af5cc5f97c00afb788ec7305dd9a58fbd16
JaganKaartik <kaartikjagan@gmail.com>
2020-10-29 21:11:22 +0530

    Rem. EJS Changes

--- app/index.ts
+++ app/index.ts
@@ -2,12 +2,9 @@ import express from 'express'
 import { rootController } from './controllers/'
 
 const app = express()
-const path = require('path')
 const port = process.env.PORT || 5000
 
-app.set('views',path.join(__dirname,'./views'))
-
-app.get('/',rootController)
+app.get('/', rootController)
 
 app.listen(port, () => {
   console.log(`App running at port: ${port}`)
