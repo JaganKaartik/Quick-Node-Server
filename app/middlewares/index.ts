3456c06c640dcfdfe7d38d0bdccd39946fdd90b2
JaganKaartik <kaartikjagan@gmail.com>
2020-11-13 23:23:17 +0530

    Session - Cookies

--- app/middlewares/index.ts
+++ app/middlewares/index.ts
@@ -3,7 +3,22 @@ import { authRouter, apiRouter } from '../routes'
 
 const Middleware = express()
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const cookieSession = require('cookie-session')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
 const cors = require('cors')
+const passportSetup = require('../config/passport-setup')
+
+Middleware.use(
+  cookieSession({
+    name: 'session',
+    keys: [process.env.COOKIE_KEY],
+    maxAge: 24 * 60 * 60 * 100
+  })
+)
+
+Middleware.use(cookieParser())
 
 Middleware.use(
   cors({
