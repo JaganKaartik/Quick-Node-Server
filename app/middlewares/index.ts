a4547d934c8b3020768dc03467f16e14ed7e3188
JaganKaartik <kaartikjagan@gmail.com>
2020-11-13 23:21:20 +0530

    Body-Parser & CORS

--- app/middlewares/index.ts
+++ app/middlewares/index.ts
@@ -2,8 +2,20 @@ import express from 'express'
 import { authRouter, apiRouter } from '../routes'
 
 const Middleware = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-Middleware.use('/api/v1', authRouter)
+Middleware.use(
+  cors({
+    orgin: 'http://localhost',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true // Allows session ookie from browser to pass through
+  })
+)
+
+Middleware.use(bodyParser.urlencoded({ extended: false }))
+Middleware.use(bodyParser.json())
 Middleware.use('/auth', authRouter)
+Middleware.use('/api/v1', authRouter)
 
 export = Middleware
