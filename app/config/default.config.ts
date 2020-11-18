eb2009457d74742fc834930c264ee95746e411ba
JaganKaartik <kaartikjagan@gmail.com>
2020-11-18 17:56:34 +0530

    Added Config

---
+++ app/config/default.config.ts
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: `.env.${process.env.APP_ENV}` })
+
+const {
+  PORT,
+  MONGO_URL,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  COOKIE_KEY,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
+} = process.env
+
+export {
+  PORT,
+  MONGO_URL,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  COOKIE_KEY,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
+}
