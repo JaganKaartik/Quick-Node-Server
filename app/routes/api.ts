fd42a2467bd326448f1111b555492f55f89541e2
JaganKaartik <kaartikjagan@gmail.com>
2020-11-18 23:34:26 +0530

    Refactored api-routes

--- app/routes/api.ts
+++ app/routes/api.ts
@@ -1,9 +1,17 @@
 import express from 'express'
-import { getController } from '../controllers/api'
+import {
+  getController,
+  postController,
+  putController,
+  deleteController
+} from '../controllers/api'
 import authCheck from '../middlewares/authCheck'
 
 const apiRouter = express.Router()
 
 apiRouter.get('/get', authCheck, getController)
+apiRouter.post('/post', authCheck, getController)
+apiRouter.delete('/delete', authCheck, getController)
+apiRouter.put('/put', authCheck, getController)
 
 export = apiRouter
